7f248052ed6e118d8135e963cff41183995066be
Fernando Lirio <89742999+FernandoLirio@users.noreply.github.com>
2024-02-22 12:05:30 -0300

    added a function find_angle

--- personal_ia.py
+++ personal_ia.py
@@ -6,6 +6,7 @@ import cv2
 import numpy as np
 import threading
 import queue
+import math
 
 class PersonalIA:
     def __init__(self, file_name = "video_frente.mp4"):
@@ -15,6 +16,22 @@ class PersonalIA:
             self.options = python.vision.PoseLandmarkerOptions(
                 base_options=python.BaseOptions(model_asset_path=self.model_path),
                 running_mode=python.vision.RunningMode.VIDEO)
+
+    def find_angle(self, frame, landsmark, p1, p2, p3, draw):
+        land = landsmark.pose_landmarks[0]
+        h, w, c = frame.shape
+        x1, y1 = (land[p1].x, land[p1].y)
+        x2, y2 = (land[p2].x, land[p2].y)
+        x3, y3 = (land[p3].x, land[p3].y)
+
+        angle = math.degrees(math.atan2(y3-y2, x3-x2) -
+                                math.atan2(y1-y2, x1-x2))
+        
+        position = (int(x2 * w + 10), int(y2 * h + 10))
+        if draw:
+            frame = cv2.putText(frame, str(int(angle)), position,
+                            cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 0), 2)
+        return frame, angle    
 
     def draw_landmarks_on_image(self, rgb_image, detection_result):
 
@@ -56,13 +73,16 @@ class PersonalIA:
                         cv2.imshow("Frame", frame)
                         if cv2.waitKey(25) & 0xFF == ord('q'):
                             break
-                    self.image_q.put((frame, detection_result))        
+
+                    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)    
+                    self.image_q.put((frame, detection_result, calc_ts))        
 
                 else:
                     break
 
             cap.release()
             cv2.destroyAllWindows()
+            self.image_q.put((1, 1, "done"))
 
     def run(self, draw, display):
         t1 = threading.Thread(target=self.process_video, args=(draw, display))
@@ -70,4 +90,4 @@ class PersonalIA:
 
 if __name__ == "__main__":
     personalia = PersonalIA()
-    personalia.process_video(True, True)
+    personalia.process_video(draw=True, display=True)
--- personalia_dash.py
+++ personalia_dash.py
@@ -5,8 +5,33 @@ from personal_ia import *
 st.set_page_config(layout="wide")
 
 personalia = PersonalIA("video_frente.mp4")
-personalia.run()
+personalia.run(draw=True, display=False)
 
+placeholder = st.empty()
+count = 0
+status = "relexed"
 while True:
-    frame, results = personalia.image_q.get()
-    st.image()
+    frame, landmarks, ts = personalia.image_q.get()
+
+    if len(landmarks.pose_landmarks) > 0:
+
+        frame, elbow_angle = personalia.find_angle(frame, landmarks, 12, 14, 16, True)
+        frame, hip_angle = personalia.find_angle(frame, landmarks, 11, 23, 25, True)
+
+        if elbow_angle > 150 and hip_angle > 170:
+            status = "ready"
+            dir = "down"
+        if status == "ready":
+            if dir == "down" and elbow_angle < 60:
+                dir = "up"
+                count += 0.5
+            if dir == "up" and elbow_angle > 100:
+                dir = "down"
+                count += 0.5
+
+        with placeholder.container():
+            col1, col2 = st.columns([0.4, 0.6])
+            col1.image(frame)
+
+            col2.markdown("### **Status:**" + status)
+            col2.markdown(f"## Count: {int(count)}")
